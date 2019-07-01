59f174eb13a4a3edcb9fa09f996d8e36204ab110
Katerina Zenchik <e.zenchik@gmail.com>
2019-07-01 14:14:33 +0300

    init: v1 of piskel-clone

---
+++ precache-manifest.a8ca75d8dc310253c9294a8f04a0725d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec71c54d184e133c9f9f8e2454cacbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4ea94a9f0cba46722f3",
+    "url": "/static/css/main.473babce.chunk.css"
+  },
+  {
+    "revision": "a4563bb37ed367b0006d",
+    "url": "/static/js/2.6ca25e21.chunk.js"
+  },
+  {
+    "revision": "d4ea94a9f0cba46722f3",
+    "url": "/static/js/main.1518ed9f.chunk.js"
+  },
+  {
+    "revision": "8abd6c0ca8e4e7b8abc8",
+    "url": "/static/js/runtime~main.4eed1d69.js"
+  }
+]);
