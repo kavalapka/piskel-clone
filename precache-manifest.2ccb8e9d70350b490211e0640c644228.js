52a34c29c48509c0e8a4d58d430a51e32d2aa317
Katerina Zenchik <e.zenchik@gmail.com>
2019-07-03 20:27:08 +0300

    add new version: piskel clone v2

---
+++ precache-manifest.2ccb8e9d70350b490211e0640c644228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e32a1eb75044d0638af548f11915b7f",
+    "url": "/piskel-clone/index.html"
+  },
+  {
+    "revision": "15d6061d3663e84575ca",
+    "url": "/piskel-clone/static/css/main.05d3eacf.chunk.css"
+  },
+  {
+    "revision": "f2a249b619656dd03cb1",
+    "url": "/piskel-clone/static/js/2.b4337f51.chunk.js"
+  },
+  {
+    "revision": "15d6061d3663e84575ca",
+    "url": "/piskel-clone/static/js/main.5f702e90.chunk.js"
+  },
+  {
+    "revision": "75a474a59554c260eb64",
+    "url": "/piskel-clone/static/js/runtime~main.c297f621.js"
+  }
+]);
